c6cc9a074a3599371a3eb259db521aeb3ab25f83
DImuthuUpe <dimuthu.upeksha2@gmail.com>
2025-02-22 09:50:29 -0500

    Rewriting the logic to support off canvas

--- RNPdfViewer/App.tsx
+++ RNPdfViewer/App.tsx
@@ -1,11 +1,12 @@
-import { AlphaType, Canvas, ColorType, Group, Image, Matrix4, multiply4, Rect, scale, SkData, Skia, SkImage, Text, translate, useTypeface } from "@shopify/react-native-skia";
+import { AlphaType, Canvas, ColorType, Group, Image, Matrix4, multiply4, Rect, scale, SkCanvas, SkData, Skia, SkImage, Text, translate, useTypeface } from "@shopify/react-native-skia";
 import { useRef, useState } from "react";
 import { Dimensions, Platform, StyleSheet, View } from "react-native";
 import { Gesture, GestureDetector, GestureHandlerRootView } from "react-native-gesture-handler";
 import { PdfiumModule } from "react-native-pdfium";
-import Animated, { useAnimatedStyle, useDerivedValue, useSharedValue, withDecay, runOnJS } from "react-native-reanimated";
+import Animated, { useAnimatedStyle, useDerivedValue, useSharedValue, withDecay, runOnJS, runOnUI } from "react-native-reanimated";
 import RNFS from 'react-native-fs';
-
+import { NitroModules } from "react-native-nitro-modules";
+import { cleanUpOutofScaleTiles, deleteAllTilesFromCacheForPage, getTileFromCache, setTileInCache } from "./src/TileCache";
 const fileName = 'sample.pdf'; // Relative to assets
 let filePath = '';
 
@@ -22,57 +23,45 @@ if (Platform.OS === 'ios') {
     });
 }
 
+
+const boxedPdfium = NitroModules.box(PdfiumModule)
 PdfiumModule.openPdf(filePath);
 
-const {width, height} = Dimensions.get('window');
+const { width: stageWidth, height: stageHeight } = Dimensions.get('screen');
 
-const TILE_SIZE = 200;
+const {width, height} = Dimensions.get('window');
+const TILE_SIZE = 256;
 const NUM_COLUMNS = Math.floor(width / TILE_SIZE);
 const NUM_ROWS = Math.floor(height / TILE_SIZE);
 const NUM_TILES = NUM_COLUMNS * NUM_ROWS;
 const PAGE_GAP = 20;
-const PAGE_COUNT = PdfiumModule.getPageCount();
-const PIXEL_ZOOM = 4;
+const PAGE_COUNT = 10;
+const PIXEL_ZOOM = 2;
 const MAX_SCALE = 3;
 
+//const tileSize = TILE_SIZE * PIXEL_ZOOM;
 
-// Create an array of tile objects with position and image URI.
-// (Replace the URI with your actual image sources.)
-const tiles = Array.from({ length: NUM_TILES * PAGE_COUNT }, (_, totalTiles) => {
-  const pageNumber = Math.trunc(totalTiles / NUM_TILES);
-  const i = totalTiles % NUM_TILES;
-  const row = Math.floor(i / NUM_COLUMNS);
-  const col = i % NUM_COLUMNS;
+const pixels = new Uint8Array(TILE_SIZE * TILE_SIZE * 4);
+    pixels.fill(255);
+    let i = 0;
+    for (let x = 0; x < TILE_SIZE; x++) {
+      for (let y = 0; y < TILE_SIZE; y++) {
+        pixels[i++] = (x * y) % 255;
+      }
+    }
 
-  return {
-    id: totalTiles,
-    row: row,
-    col: col,
-    x: col * TILE_SIZE,
-    y: row * TILE_SIZE + (pageNumber * NUM_ROWS * TILE_SIZE + PAGE_GAP * pageNumber),
-    width: TILE_SIZE,
-    height: TILE_SIZE,
-    pageNumber: pageNumber,
-    color: i % 2 === 0 ? "red" : "blue",
-  };
-});
-
-function App(): React.JSX.Element {
-
-  const origin = useSharedValue({ x: 0, y: 0 });
-  const offset = useSharedValue(Matrix4());
+    
+const App = () => {
 
   const offsetX = useSharedValue<number>(0);
   const offsetY = useSharedValue<number>(0);
-  const scaleVal = useSharedValue<number>(1);
-  
+  const origin = useSharedValue({ x: 0, y: 0 });
+  const offset = useSharedValue(Matrix4());
   const matrix = useSharedValue(Matrix4());
-
-  const [visiblePage, setVisiblePage] = useState(0);
-
-  const tileDataCache = useRef(new Map<number, SkImage>());
+  const scaleEndValue = useSharedValue<number>(1);
 
   const panGesture = Gesture.Pan().onChange((e) => {
+
     offsetX.value += e.changeX;
     offsetY.value += e.changeY;
 
@@ -86,8 +75,10 @@ function App(): React.JSX.Element {
     });
   });
 
+
   const pinchGesture = Gesture.Pinch()
   .onBegin((e) => {
+      
       origin.value = { x: e.focalX, y: e.focalY };
       offset.value = matrix.value;
   })
@@ -99,78 +90,148 @@ function App(): React.JSX.Element {
       offset.value,
       scale(e.scale, e.scale, 1, origin.value)
     );
-    scaleVal.value = e.scale;
+  }).onEnd((e) => {
+    scaleEndValue.value = e.scale;
   });
 
+  const getTileFromPdfium = (page: number, row: number, col: number, zoomFactor: number) => {
+    "worklet";
+    const tileBuf = boxedPdfium.unbox().getTile(page, -row, -col, width / 2, TILE_SIZE * zoomFactor, zoomFactor);
 
-  const getTile = (tileid: number, tileRow: number, tileCol: number, scale: number, pageNumber: number, tileSize: number, pageWidth: number) => {
-
-    const cacheEntry = tileDataCache.current.get(tileid)
-    if (cacheEntry) {
-      return cacheEntry;
-    }
-  
-    const buf = PdfiumModule.getTile(pageNumber, -tileRow, -tileCol, pageWidth, tileSize * PIXEL_ZOOM, scale * PIXEL_ZOOM);
-    const ints = new Uint8Array(buf);
-
-    const data = Skia.Data.fromBytes(ints);
-    const image = Skia.Image.MakeImage(
+    const data = Skia.Data.fromBytes(new Uint8Array(tileBuf));
+    const img = Skia.Image.MakeImage(
       {
-          width: tileSize * PIXEL_ZOOM,
-          height: tileSize * PIXEL_ZOOM,
-          alphaType: AlphaType.Opaque,
-          colorType: ColorType.RGBA_8888,
+        width: TILE_SIZE * zoomFactor,
+        height: TILE_SIZE * zoomFactor,
+        alphaType: AlphaType.Opaque,
+        colorType: ColorType.BGRA_8888,
       },
       data,
-      tileSize * PIXEL_ZOOM * 4);
+      TILE_SIZE * zoomFactor * 4
+    );
     data.dispose();
-    tileDataCache.current.set(tileid, image as SkImage);
+    return img;
+  }
 
-    if (tileDataCache.current.size > 100) {
-      tileDataCache.current.forEach((value, key) => {
-        if (Math.abs(key - tileid) > 100) {
-          console.log('disposing', key);
-          value.dispose();
-          tileDataCache.current.delete(key);
-          console.log(HermesInternal.getInstrumentedStats())
-        }
-      });
+  const drawTile = (canvas: SkCanvas, offX: number, offY: number, 
+    scale: number, page: number, col: number, row: number) => {
+
+    "worklet";
     
+    const zoomFactor = PIXEL_ZOOM * scale;
+    canvas.translate((col * TILE_SIZE + offX) * zoomFactor, 
+                     (row * TILE_SIZE + offY) * zoomFactor);
+
+    const gridLocation = `${col}-${row}`;
+
+    var img = getTileFromCache(page, scale, gridLocation);
+                 
+    if (img == null) {      
+      img = getTileFromPdfium(page, row, col, zoomFactor);
+      setTileInCache(page, scale, gridLocation, img);
     }
 
-    return image;
+    canvas.drawImage(img as SkImage,  0,  0, );
+    canvas.translate(- (col * TILE_SIZE + offX) * zoomFactor, -(offY + row * TILE_SIZE) * zoomFactor);
+  }
+
+  const drawTiles = (canvas: SkCanvas, offX: number, offY: number, scale: number, pageNum: number) => {
+    'worklet';
+
+    canvas.save();
+    
+    drawTile(canvas, offX, offY, scale, pageNum, 0, 0);
+    drawTile(canvas, offX, offY, scale, pageNum, 1, 0);
+    drawTile(canvas, offX, offY, scale, pageNum, 2, 0);
+    drawTile(canvas, offX, offY, scale, pageNum, 0, 1);
+    drawTile(canvas, offX, offY, scale, pageNum, 1, 1);
+    drawTile(canvas, offX, offY, scale, pageNum, 2, 1);
+    drawTile(canvas, offX, offY, scale, pageNum, 0, 2);
+    drawTile(canvas, offX, offY, scale, pageNum, 1, 2);
+    drawTile(canvas, offX, offY, scale, pageNum, 2, 2);
+
+    drawTile(canvas, offX, offY, scale, pageNum, 0, 3);
+    drawTile(canvas, offX, offY, scale, pageNum, 1, 3);
+    drawTile(canvas, offX, offY, scale, pageNum, 2, 3);
+    canvas.restore();
+
+
   };
-  
+
+  const getOffScreenPage = (pageNum: number, yOff: number, scale: number) => {
+    "worklet";
+
+    const pageByOffset = Math.ceil(-yOff / (1050 * scale));
+    if ((pageNum - pageByOffset) > (2 / scale)) {
+      deleteAllTilesFromCacheForPage(pageNum);
+      return null;
+    }  
+
+    scale = Math.ceil(scale);
+    
+    cleanUpOutofScaleTiles(pageNum,scale);
+
+    if (global.pageCache == null) {
+      global.pageCache = {};
+    }
+
+    if (global.pageCache[pageNum] == null) {
+      global.pageCache[pageNum] = {};
+    }
+
+
+    const keys = Object.keys(global.pageCache[pageNum]);
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i] != scale) {
+          global.pageCache[pageNum][keys[i]].dispose();
+          delete global.pageCache[pageNum][keys[i]];
+          global.gc();
+        }
+    }
+
+    if (global.pageCache[pageNum][scale] == null) {
+      const offscreen = Skia.Surface.MakeOffscreen(stageWidth * PIXEL_ZOOM * scale, stageHeight * PIXEL_ZOOM * scale)!;
+      
+      const canvas = offscreen.getCanvas();
+      drawTiles(canvas, 0, 0, scale, pageNum);
+      global.gc();
+      global.pageCache[pageNum][scale] = offscreen.makeImageSnapshot();
+    }
+
+    
+    return global.pageCache[pageNum][scale];
+  };
+
+
   const animatedMat = useDerivedValue(() => {
     // Update the matrix with decaying values. https://github.com/wcandillon/can-it-be-done-in-react-native/issues/174
     // This is the hook to re-render canvas when the page changes.
-    runOnJS(setVisiblePage)(-Math.ceil(offsetY.value / (scaleVal.value * (NUM_ROWS * TILE_SIZE + PAGE_GAP))));
     return multiply4(translate(offsetX.value, offsetY.value), matrix.value);
   });
 
-  return (  
+  return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
-          <Canvas style={{ flex: 1, backgroundColor: 'gray' }}> 
-            <Group matrix={animatedMat}>
-              {tiles.map((tile) => {
-
-                if (Math.abs(tile.pageNumber - visiblePage) > 2) {
-                  return null;
-                }
-                return (
-                  <Image key={tile.id} x={tile.x} y={tile.y} width={tile.width} height={tile.height} 
-                    image={getTile(tile.id, tile.row, tile.col, 0.5, tile.pageNumber, TILE_SIZE, width)! as SkImage} />
-              )})}
-            </Group>
-          </Canvas>
-        
+        <Canvas
+        style={{
+          width: stageWidth,
+          height: stageHeight,
+          backgroundColor: 'lightblue',
+        }}
+        > 
+          <Group matrix={animatedMat}>
+            {[...Array(PAGE_COUNT)].map((_, i) => (
+              <Image key={i} image={useDerivedValue(() => getOffScreenPage(i, offsetY.value, scaleEndValue.value))} y={1050 * i} width={stageWidth} height={stageHeight} />
+            ))}
+          </Group>  
+        </Canvas>
         <GestureDetector gesture={Gesture.Race(pinchGesture, panGesture)}>
           <Animated.View style={[StyleSheet.absoluteFill]} />
         </GestureDetector>
       </View>
-    </GestureHandlerRootView>);
-  
+    </GestureHandlerRootView>
+    
+  );
 };
 
 export default App;
---
+++ RNPdfViewer/src/TileCache.ts
@@ -0,0 +1,72 @@
+import { SkImage } from "@shopify/react-native-skia";
+
+
+const initTileCache = (page: number, scale: number) => {
+    "worklet";
+    if (global.skImageCache == null) {
+        global.skImageCache = {};
+    }
+
+    if (global.skImageCache[page] == null) {
+    global.skImageCache[page] = {};
+    }  
+
+    if (global.skImageCache[page][scale] == null) {
+    global.skImageCache[page][scale] = {};
+    }  
+}
+export const getTileFromCache = (page: number, scale: number, gridLocation: string) => {
+    "worklet";
+    initTileCache(page, scale);
+    return global.skImageCache[page][scale]?.[gridLocation];
+  }
+
+export const setTileInCache = (page: number, scale: number, gridLocation: string, img: SkImage) => {
+    "worklet";
+    initTileCache(page, scale); 
+    global.skImageCache[page][scale][gridLocation] = img;
+}
+
+export const cleanUpOutofScaleTiles = (page: number, currentScale: number) => {
+    "worklet";
+    if (global.skImageCache == null) {
+        return
+    }
+
+    if (global.skImageCache[page] == null) {
+        return
+    }
+
+    const scales = Object.keys(global.skImageCache[page]);
+    for (let i = 0; i < scales.length; i++) {
+        if (scales[i] != currentScale) {
+            const gridLocations = Object.keys(global.skImageCache[page][scales[i]]);
+            for (let j = 0; j < gridLocations.length; j++) {
+                global.skImageCache[page][scales[i]][gridLocations[j]].dispose();
+                delete global.skImageCache[page][scales[i]][gridLocations[j]];
+            }
+            delete global.skImageCache[page][scales[i]];
+        }
+    }
+}
+
+export const deleteAllTilesFromCacheForPage = (page: number) => {
+    "worklet";
+    if (global.skImageCache == null) {
+      return
+    }
+    if (global.skImageCache[page] != null) {
+        const scales = Object.keys(global.skImageCache[page]);
+        for (let i = 0; i < scales.length; i++) {
+
+            const gridLocations = Object.keys(global.skImageCache[page][scales[i]]);
+            for (let j = 0; j < gridLocations.length; j++) {
+                global.skImageCache[page][scales[i]][gridLocations[j]].dispose();
+                delete global.skImageCache[page][scales[i]][gridLocations[j]];
+            }
+            delete global.skImageCache[page][scales[i]];
+        }
+        delete global.skImageCache[page];
+        global.gc();
+    }
+}   
